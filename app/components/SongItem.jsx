2567e3bd5773a59da125ff7ae5f9307c6e01d45d
ApP0D <u6510550@au.edu>
2024-09-29 16:17:46 +0700

    fix delete

--- app/components/SongItem.jsx
+++ app/components/SongItem.jsx
@@ -1,20 +1,42 @@
 // components/SongItem.jsx
 
-import React from 'react';
-import { Button } from '@mui/material'; // Import MUI Button
-import styles from '@/app/page.module.css';
+import React from "react";
+import { Button } from "@mui/material"; // Import MUI Button
+import styles from "@/app/page.module.css";
 
 const SongItem = ({ song, onEdit, onDelete }) => {
   return (
     <li className={styles.songItem}>
-      <p>{song.title} by {song.artist}</p>
-      <Button variant="outlined" color="primary" onClick={onEdit} style={{ marginRight: '8px' }}>
+      <p>
+        {song.title} by {song.artist}
+      </p>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={onEdit}
+        style={{ marginRight: "8px" }}
+      >
         Edit
       </Button>
-      <Button
+      {/* <Button
         variant="outlined"
         color="secondary"
         onClick={() => onDelete(song._id)}
+        style={{ marginRight: '8px' }}
+      >
+        Delete
+      </Button> */}
+      <Button
+        variant="outlined"
+        color="error" // Use MUI's error color for consistent styling
+        onClick={() => onDelete(song._id)}
+        sx={{
+          borderColor: "red", // or your preferred color
+          color: "red",
+          "&:hover": {
+            backgroundColor: "rgba(255, 0, 0, 0.1)", // Light red background on hover
+          },
+        }}
       >
         Delete
       </Button>
